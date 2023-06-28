fa12da2ba0adf21467d9c81d6082b903a998ce30
Arthurito <90821581+xcrisarthur@users.noreply.github.com>
2023-06-28 22:42:48 +0700

    up

--- src/Pages/Merchandise.jsx
+++ src/Pages/Merchandise.jsx
@@ -1,11 +1,42 @@
+import React, { useEffect, useRef } from 'react';
 import Footer from "../Components/Footer";
 import NavigationBar from "../Components/NavigationBar";
+import 'jquery';
+import Flickity from 'flickity';
+import 'flickity/css/flickity.css';
+import "../Css/style.css";
 
 const Merchandise = () => {
+  const carouselRef = useRef(null);
+
+  useEffect(() => {
+    const flickity = new Flickity(carouselRef.current, {
+      wrapAround: true // Contoh konfigurasi Flickity
+    });
+
+    // Bersihkan Flickity saat komponen dibongkar
+    return () => {
+      flickity.destroy();
+    };
+  }, []);
+
   return (
     <>
       <NavigationBar />
-      <h1>Merchandise</h1>
+
+      <div className="my-6">
+        <h1 className="text-center font-bold text-2xl">SEMAFIT STORE</h1>
+        <h2 className="text-center font-semibold text-xl">@toko.semafit</h2>
+      </div>
+
+      <div className="gallery js-flickity" ref={carouselRef}>
+        <div className="gallery-cell"></div>
+        <div className="gallery-cell"></div>
+        <div className="gallery-cell"></div>
+        <div className="gallery-cell"></div>
+        <div className="gallery-cell"></div>
+      </div>
+
       <Footer/>
     </>
   );
